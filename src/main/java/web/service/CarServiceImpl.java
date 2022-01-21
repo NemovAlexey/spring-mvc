87f417d97e1720a1cc5337da218cdf145ed76b77
Nemov Aleksei <nemov90@mail.ru>
2022-01-21 16:59:53 +0300

    some edits

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,16 +1,16 @@
 package web.service;
 
 import org.springframework.stereotype.Service;
-import web.dao.CarDaoImpl;
+import web.dao.CarDao;
 import web.model.Car;
 
 import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
-    private final CarDaoImpl carDao;
+    private final CarDao carDao;
 
-    public CarServiceImpl(CarDaoImpl carDao) {
+    public CarServiceImpl(CarDao carDao) {
         this.carDao = carDao;
     }
 
